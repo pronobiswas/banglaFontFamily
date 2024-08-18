05c5c05e44cfacbc53392181eaa5bda2ee6f6c26
PRONOB BISWAS <biswaspronob@outlook.com>
2024-08-19 02:26:40 +0600

    all free banglafont available in lipighar

---
+++ script.js
@@ -0,0 +1,238 @@
+const fonts = [
+  {
+    name: "LiAlinurShishir",
+    url: "fonts/LiAlinurShishirUnicode.ttf",
+  },
+  {
+    name: "LiAlinurAkorshon",
+    url: "fonts/LiAlinurAkorshonUnicode.ttf",
+  },
+  {
+    name: "LiSweetShreyamUnicode",
+    url: "fonts/LiSweetShreyamUnicode.ttf",
+  },
+  {
+    name: "PotroSansBanglaRegular",
+    url: "fonts/PotroSansBanglaRegular.ttf",
+  },
+  {
+    name: "LiShohidAbuSayedUnicode",
+    url: "fonts/LiShohidAbuSayedUnicode.ttf",
+  },
+  {
+    name: "LiAlinurNakkhatraUnicode",
+    url: "fonts/LiAlinurNakkhatraUnicode.ttf",
+  },
+  {
+    name: "LiSweetRupamUnicode",
+    url: "fonts/LiSweetRupamUnicode.ttf",
+  },
+  {
+    name: "LiHalderChinmoyeeUnicode",
+    url: "fonts/LiHalderChinmoyeeUnicode.ttf",
+  },
+  {
+    name: "LiAlinurKuyashaUnicode",
+    url: "fonts/LiAlinurKuyashaUnicode.ttf",
+  },
+  {
+    name: "LiAlinurBahubrihiUnicode",
+    url: "fonts/LiAlinurBahubrihiUnicode.ttf",
+  },
+  {
+    name: "LiAlinurAkorshonUnicode",
+    url: "fonts/LiAlinurAkorshonUnicode.ttf",
+  },
+  {
+    name: "LiAlinurBanglabornoUnicode",
+    url: "fonts/LiAlinurBanglabornoUnicode.ttf",
+  },
+  {
+    name: "LiAlinurProhorUnicode",
+    url: "fonts/LiAlinurProhorUnicode.ttf",
+  },
+  {
+    name: "LiAlinurTatsamaUnicode",
+    url: "fonts/LiAlinurTatsamaUnicode.ttf",
+  },
+  {
+    name: "LiAlinurSangbadpatraUnicode",
+    url: "fonts/LiAlinurSangbadpatraUnicode.ttf",
+  },
+  {
+    name: "LiAlinurRuposhiUnicode",
+    url: "fonts/LiAlinurRuposhiUnicode.ttf",
+  },
+  {
+    name: "LiSweetShreyamUnicode",
+    url: "fonts/LiSweetShreyamUnicode.ttf",
+  },
+  {
+    name: "LiSweetRupamUnicode",
+    url: "fonts/LiSweetRupamUnicode.ttf",
+  },
+  {
+    name: "LiSabbirAshalota3Unicode",
+    url: "fonts/LiSabbirAshalota3Unicode.ttf",
+  },
+  {
+    name: "LiMahfuzHimadriUnicode",
+    url: "fonts/LiMahfuzHimadriUnicode.ttf",
+  },
+  {
+    name: "LiManowarNiharUnicode",
+    url: "fonts/LiManowarNiharUnicode.ttf",
+  },
+  {
+    name: "LiSirajeeSanjarUnicode",
+    url: "fonts/LiSirajeeSanjarUnicode.ttf",
+  },
+  {
+    name: "LiAlinurSmaroklipiUnicode",
+    url: "fonts/LiAlinurSmaroklipiUnicode.ttf",
+  },
+  {
+    name: "LiSKBishalUnicode",
+    url: "fonts/LiSKBishalUnicode.ttf",
+  },
+  {
+    name: "LiAlinurRangpurUnicode",
+    url: "fonts/LiAlinurRangpurUnicode.ttf",
+  },
+  {
+    name: "LiDurbinitoUnicode",
+    url: "fonts/LiDurbinitoUnicode.ttf",
+  },
+  {
+    name: "LiHalderSourikUnicode",
+    url: "fonts/LiHalderSourikUnicode.ttf",
+  },
+  {
+    name: "LiSuparnaUnicode",
+    url: "fonts/LiSuparnaUnicode.ttf",
+  },
+  {
+    name: "LiAlinurAtithiUnicode",
+    url: "fonts/LiAlinurAtithiUnicode.ttf",
+  },
+  {
+    name: "LiManowarMurshidabadUnicode",
+    url: "fonts/LiManowarMurshidabadUnicode.ttf",
+  },
+  {
+    name: "LiAlinurImaratUnicode",
+    url: "fonts/LiAlinurImaratUnicode.ttf",
+  },
+  {
+    name: "LiAlinurSnigdhaUnicode",
+    url: "fonts/LiAlinurSnigdhaUnicode.ttf",
+  },
+  {
+    name: "LiFazlayMunnishaMattraUnicodeMattra",
+    url: "fonts/LiFazlayMunnishaMattraUnicodeMattra.ttf",
+  },
+  {
+    name: "LiNahidaBornomalaUnicode",
+    url: "fonts/LiNahidaBornomalaUnicode.ttf",
+  },
+  {
+    name: "LiSuparnaUnicode1",
+    url: "fonts/LiSuparnaUnicode1.ttf",
+  },
+  {
+    name: "LiAlinurAdorUnicode",
+    url: "fonts/LiAlinurAdorUnicode.ttf",
+  },
+  {
+    name: "LiByomgraphyKoushaliUnicode",
+    url: "fonts/LiByomgraphyKoushaliUnicode.ttf",
+  },
+  {
+    name: "LiAlinurProttyoeeUnicode",
+    url: "fonts/LiAlinurProttyoeeUnicode.ttf",
+  },
+  {
+    name: "LiAlinurBornoBikashUnicode",
+    url: "fonts/LiAlinurBornoBikashUnicode.ttf",
+  },
+  {
+    name: "LiManowarMatribhumiUnicode",
+    url: "fonts/LiManowarMatribhumiUnicode.ttf",
+  },
+  {
+    name: "LiAlinurKurigramUnicode",
+    url: "fonts/LiAlinurKurigramUnicode.ttf",
+  },
+  {
+    name: "LiLiakatTanjinaUnicodeRegular",
+    url: "fonts/LiLiakatTanjinaUnicodeRegular.ttf",
+  },
+  {
+    name: "LiPadmasetuUnicode",
+    url: "fonts/LiPadmasetuUnicode.ttf",
+  },
+  {
+    name: "LiAlinurDristantoUnicode-Regular",
+    url: "fonts/LiAlinurDristantoUnicode-Regular.ttf",
+  },
+  {
+    name: "LiMainakBuniyadiUnicode",
+    url: "fonts/LiMainakBuniyadiUnicode.ttf",
+  },
+  {
+    name: "LiAlinurProjapotiUnicode",
+    url: "fonts/LiAlinurProjapotiUnicode.ttf",
+  },
+  {
+    name: "LiManowarFingeUnicode",
+    url: "fonts/LiManowarFingeUnicode.ttf",
+  },
+  {
+    name: "LiPriyashaBharotUnicode",
+    url: "fonts/LiPriyashaBharotUnicode.ttf",
+  },
+  {
+    name: "LiAlinurShowpnocari",
+    url: "fonts/LiAlinurShowpnocari.ttf",
+  },
+  {
+    name: "LiLiakatTawhidUnicode",
+    url: "fonts/LiLiakatTawhidUnicode.ttf",
+  },
+  {
+    name: "LiParadoxicalSajidUnicode",
+    url: "fonts/LiParadoxicalSajidUnicode.ttf",
+  },
+  {
+    name: "LiAlinurIchamatiUnicode",
+    url: "fonts/LiAlinurIchamatiUnicode.ttf",
+  },
+  {
+    name: "LiAlinurAbakasUnicode",
+    url: "fonts/LiAlinurAbakasUnicode.ttf",
+  },
+  {
+    name: "LiHasanIccheghuriUnicode-Regular",
+    url: "fonts/LiHasanIccheghuriUnicode-Regular.ttf",
+  },
+  {
+    name: "LiAdorNoirritSemiBold",
+    url: "fonts/LiAdorNoirritSemiBold.ttf",
+  },
+  {
+    name: "LiAlinurBoisakhUnicode-Regular",
+    url: "fonts/LiAlinurBoisakhUnicode-Regular.ttf",
+  },
+  {
+    name: "LiShadhinata2Unicode-Regular",
+    url: "fonts/LiShadhinata2Unicode-Regular.ttf",
+  },
+  {
+    name: "LiHimaloyUnicode-Regular",
+    url: "fonts/LiHimaloyUnicode-Regular.ttf",
+  },
+  // ===============page seven swadhinota======  
+  
+
+
+];
